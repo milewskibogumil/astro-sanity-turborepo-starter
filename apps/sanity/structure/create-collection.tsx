6264676b4136eeb89376aef63c38e03775d9b466
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-09-21 14:55:32 +0200

    add id for createCollection sanity builder

--- apps/sanity/structure/create-collection.tsx
+++ apps/sanity/structure/create-collection.tsx
@@ -1,11 +1,12 @@
 import type { StructureBuilder } from "sanity/structure";
 import { schemaTypes } from "./schema-types";
-import { TYPES_TO_EXCLUDE_PREVIEWS } from ".";
 import { Preview } from "./preview";
+import { TYPES_TO_EXCLUDE_PREVIEWS } from ".";
 
 export const createCollection = (S: StructureBuilder, name: string) => {
   const { title, icon } = schemaTypes.find(item => item.name === name) as { title: string, icon: React.ReactNode };
   return S.listItem()
+    .id(name)
     .title(title)
     .icon(icon)
     .child(
