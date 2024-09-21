bf677f5f85c95f6c75024698544765ef2ac76c64
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-09-21 10:18:59 +0200

    fix redirects logic for astro to handle 0 redirects

--- apps/astro/redirects.ts
+++ apps/astro/redirects.ts
@@ -16,16 +16,16 @@ const data = await sanityFetch<RedirectData[]>({
     }[]
   `
 });
-const redirects = Object.fromEntries(
+const redirects = data ? Object.fromEntries(
   data.map(({ source, destination, isPermanent }) => [
     source, {
       status: (isPermanent ? 301 : 302) as ValidRedirectStatus,
       destination
     }
   ])
-);
-const permanentRedirects = data.filter(r => r.isPermanent).length;
-const temporaryRedirects = data.length - permanentRedirects;
+) : {};
+const permanentRedirects = data ? data.filter(r => r.isPermanent).length : 0;
+const temporaryRedirects = data ? data.length - permanentRedirects : 0;
 console.log('\x1b[32m%s\x1b[0m', `✅ \x1b[1m${Object.keys(redirects).length}\x1b[0m\x1b[32m redirects added from Sanity (\x1b[1m${permanentRedirects}\x1b[0m\x1b[32m permanent and \x1b[1m${temporaryRedirects}\x1b[0m\x1b[32m temporary)`);
 
 export default redirects;
